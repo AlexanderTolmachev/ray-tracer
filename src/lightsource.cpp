ba9905a05d12216db9a5d11006c041e98900a3b4
Alexander Tolmachev <alexander.s.tolmachev@gmail.com>
2013-04-28 03:40:40 +0400

    Added lightsource definition

---
+++ src/lightsource.cpp
@@ -0,0 +1,14 @@
+/*!
+ *\file lightsource.cpp
+ *\brief Contains LightSource class definition
+ */
+
+#include "lightsource.h"
+#include "scene.h"
+
+
+Color LightSource::calculateColor(const Scene &scene, ShapePointer shape, const Ray &ray, float distance, const Vector &normal) const {
+  Color color;
+  // TODO implement
+  return color;
+}
--- src/lightsource.h
+++ src/lightsource.h
@@ -7,7 +7,12 @@
 
 #include <QSharedPointer>
 
+#include "types.h"
+#include "shape.h"
+#include "ray.h"
+
 struct LightSource;
+class Scene;
 
 typedef QSharedPointer<LightSource> LightSourcePointer;
 
@@ -19,5 +24,33 @@ enum LightSourceType
 };
 
 struct LightSource {
+  Color calculateColor(const Scene &scene, ShapePointer shape, const Ray &ray, float distance, const Vector &normal) const;
 
+  // Type of the light source
+  LightSourceType type;
+
+  // Light position (for spot and point lights)
+  Vector position;
+  // Light direction (for spot and direction lights)
+  Vector direction;
+ 
+  // Colors intensity
+  Color	ambientIntensity;
+  Color	diffuseIntensity;
+  Color specularIntensity;
+
+  // Attenuation coefficients
+  float	constantAttenutaionCoefficient;
+  float linearAttenutaionCoefficient;
+  float quadraticAttenutaionCoefficient;
+
+  // Penumbra angle in radians (for spot light)
+  float	penumbraAngle;
+  // Umbra angle in radians (for spot ligh)
+  float	umbraAngle;
+  // Falloff factor (for spotlight)
+  float	falloffFactor;
+
+  // Light range (for directional light)
+  float	range;
 };
